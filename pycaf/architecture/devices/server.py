fa4b4a72d11f38494d551f956c1b98bf15030cb8
Maxime Olivier <maxime.olivier@amossys.fr>
2015-06-10 23:32:56 +0200

    Ajout des setters pour les serveurs

--- pycaf/architecture/devices/server.py
+++ pycaf/architecture/devices/server.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 #| This file is part of pyCAF.                                               |
 #|                                                                           |
 #| pyCAF is free software: you can redistribute it and/or modify             |
@@ -13,7 +15,6 @@
 #| You should have received a copy of the GNU General Public License         |
 #| along with this program. If not, see <http://www.gnu.org/licenses/>.      |  
 
-# -*- coding: utf-8 -*-
 
 import server_features as sf
 
@@ -29,6 +30,8 @@ class Server():
 
     def __init__(self, servername=None, ostype=None, osname=None, osversion=None, osarchitecture = None, kernel_release = None, kernel_version_running = None, accountslist=None,\
     processeslist=None, connectionslist=None, interfaceslist=None, packageslist=None, version_script=None):
+        """ Objet serveur
+        """
         self.name = servername
         self.ostype = ostype
         self.osname = osname
@@ -36,8 +39,6 @@ class Server():
         self.osarchitecture = osarchitecture
         self.kernel_release = kernel_release
         self.kernel_version_running = kernel_version_running
-        self.kernel_version_installed = None
-        self.kernel_version_uptodate = None
 
         self.accounts = accountslist
         self.processes = processeslist
@@ -45,7 +46,7 @@ class Server():
         self.interfaces = interfaceslist
         self.packages = packageslist
         self.version_script = version_script
-        
+
         # Packages analysis
         self.nb_packages = -1
         self.nb_packages_uptodate = -1
@@ -54,14 +55,16 @@ class Server():
         self.packages_uptodate = sf.PackageList()
         self.packages_obsolete = sf.PackageList()
         self.packages_unchecked = sf.PackageList()
-        
+
+        self.kernel_version_installed = None
+        self.kernel_version_uptodate = None
         # Kernel can have 4 states :
         # 1 : Critical state : the release is obsolete
         # 2 : Warning state : the version of the kernel is obsolete (just update package)
         # 3 : Warning state 2 : the right version is installed but not running. Need to reboot the PC.
         # 4 : Clear state : the kernel release and version are up to date
         self.kernel_result = -1
-        
+
         # Analyze processes
         # Listening and established connections
         self.listening_connections = sf.ConnectionList()
@@ -69,37 +72,72 @@ class Server():
         self.root_processes = sf.ProcessList()
         # Listening connection whose user is root
         self.listening_root_process = sf.ProcessList()
-        
+
         # Files lists
         self.files = sf.FileList()
         self.files_potentially_threat = sf.FileList()
-        
+
         # PAM conf (string contains utility configuration)
         self.pam = ""
-        
+
         # nsswictch conf (string contains utility configuration)
         self.nsswitch = ""
-        
+
         # SSH config object (see server features objects)
         self.ssh_config = sf.SSHConfig()
         
         # Suoders config
         self.sudoers_config = ""
-        
+
         # fstab config
         self.fstab_config = ""
-        
+
         # crontab config dictionnary
         #{"USER","crontab rules1 \nrules2 \nrules3"}
         self.crontab_config = {}
         self.crontab_rules_counter = 0
         # Crontab files analysis results. [[user,file_path,ok], [user,file_path,ko], [user,file_path,ok], ...]
         self.crontab_results = []
-        
+
         # IP-hostname dictionnaries
         self.ip_hostname_local = {}
         # Common dictionnay when several servers are imported
         self.ip_hostname_common = {}
+
+    def set_name(self, name):
+        """ Setter pour la propriété name d'un serveur
+        """
+        self.name = name
+
+    def set_ostype(self, ostype):
+        """ Setter pour la propriété ostype d'un serveur
+        """
+        self.ostype = ostype
+
+    def set_osname(self, osname):
+        """ Setter pour la propriété osname d'un serveur
+        """
+        self.osname = osname
+
+    def set_osversion(self, osversion):
+        """ Setter pour la propriété osversion d'un serveur
+        """
+        self.osversion = osversion
+
+    def set_osarchitecture(self, osarchitecture):
+        """ Setter pour la propriété osarchitecture d'un serveur
+        """
+        self.osarchitecture = osarchitecture
+
+    def set_kernel_release(self, kernel_release):
+        """ Setter pour la propriété kernel_release d'un serveur
+        """
+        self.kernel_release = kernel_release
+
+    def set_kernel_version_running(self, kernel_version_running):
+        """ Setter pour la propriété kernel_version_running d'un serveur
+        """
+        self.kernel_version_running = kernel_version_running
         
     def register_raw_info_in_files(self, path = None):
         """
